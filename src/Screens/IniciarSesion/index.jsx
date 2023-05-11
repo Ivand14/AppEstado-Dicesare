27f7378029b8e137ae0c95ee1d67f3192ab58b45
Ivan <carivan14@homtail.com>
2023-05-11 15:38:15 -0300

    Añadiendo Navegaciones

--- src/Screens/IniciarSesion/index.jsx
+++ src/Screens/IniciarSesion/index.jsx
@@ -5,8 +5,10 @@ import Card from "../../components/Card";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { ImageBackground } from "react-native";
 import Input from "../../components/Input";
+import { Keyboard } from "react-native";
 import { KeyboardAvoidingView } from "react-native";
 import React from "react";
+import { TouchableWithoutFeedback } from "react-native";
 import styles from "./styles";
 
 const IniciarSesion = () => {
@@ -17,21 +19,23 @@ const IniciarSesion = () => {
     return (
     <ImageBackground source={imgUrl} style={styles.ImageBackground} >
     
-        <KeyboardAvoidingView behavior="height">
-            <Card otherStyles={styles.CardLogin}>
+        <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
+            <KeyboardAvoidingView behavior="height">
+                <Card otherStyles={styles.CardLogin}>
 
-                <View style={styles.containerInputIcon}>
-                    <Icon name="user" size={30} color={'#fff'} style={styles.Icon}/>
-                    <Input placeholder='Email' autoComplete='email' newStyle={styles.Input}  />                   
-                </View>
+                    <View style={styles.containerInputIcon}>
+                        <Icon name="user" size={30} color={'#fff'} style={styles.Icon}/>
+                        <Input placeholder='Email' autoComplete='email' newStyle={styles.Input}  />                   
+                    </View>
 
-                <View style={styles.containerInputIcon}>
-                    <Icon name="lock" size={30} color={'#fff'} style={styles.Icon}/>
-                    <Input placeholder='Password' autoComplete='password' secureTextEntry={true} newStyle={styles.Input} />                                         
-                </View>
-                <Boton newStyles={styles.BotonLogin}>INICIAR SESION</Boton>
-            </Card>
-        </KeyboardAvoidingView>
+                    <View style={styles.containerInputIcon}>
+                        <Icon name="lock" size={30} color={'#fff'} style={styles.Icon}/>
+                        <Input placeholder='Password' autoComplete='password' secureTextEntry={true} newStyle={styles.Input} />                                         
+                    </View>
+                    <Boton newStyles={styles.BotonLogin}>INICIAR SESION</Boton>
+                </Card>
+            </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
     
     </ImageBackground>
     );
